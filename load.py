99646f9fdbc60fde23f5ad6054174d01f03e3f97
rafael scariott <rafa.scariott@gmail.com>
2023-10-16 20:22:00 -0300

    funcional

--- load.py
+++ load.py
@@ -12,39 +12,39 @@ from google.colab import auth
 from oauth2client.client import GoogleCredentials
 
 
-class Load():
-    def load_from_drive():
-        try:
-            # Authenticate and create the PyDrive client.
-            auth.authenticate_user()
-            gauth = GoogleAuth()
-            gauth.credentials = GoogleCredentials.get_application_default()
-            drive = GoogleDrive(gauth)
-            link = 'https://drive.google.com/file/d/1_lHw76vKTlZKbKvf34HqpoLrY83LDHLl/view?usp=sharing'
-            fileDownloaded = drive.CreateFile({'id':'1_lHw76vKTlZKbKvf34HqpoLrY83LDHLl'})
-            fileDownloaded.GetContentFile('Crimes_-_2022 (1).csv')
-            df_crime = pd.read_csv('Crimes_-_2022 (1).csv', header=0, low_memory=False)
-        except:
-            print("Carga não conseguiu ser feita")
-    
-    def clean_data():
-        # Vamos dropar as colunas X Coordinate, Y Coordinate, Latitude e Longitude
-        df_crime = df_crime.drop(['Latitude', 'Longitude', 'X Coordinate', 'Y Coordinate', 'Year'], axis=1)
-        # Limpar a coluna Location pois estava vindo com NaN
-        df_crime = df_crime.dropna(subset=['Location'], how='any')
-        df_crime
-        # dim_Time = df[['Date']].drop_duplicates()
-        # dim_Time = dim_Time.sort_values(by='Date', ascending=True)
 
-        # id_Time = []
-        # id = 1
-        # for i in range(len(dim_Time)):
-        #     id_Time.append(id)
-        #     id += 1
+def load_from_drive():
+    try:
+        # Authenticate and create the PyDrive client.
+        auth.authenticate_user()
+        gauth = GoogleAuth()
+        gauth.credentials = GoogleCredentials.get_application_default()
+        drive = GoogleDrive(gauth)
+        link = 'https://drive.google.com/file/d/1_lHw76vKTlZKbKvf34HqpoLrY83LDHLl/view?usp=sharing'
+        fileDownloaded = drive.CreateFile({'id':'1_lHw76vKTlZKbKvf34HqpoLrY83LDHLl'})
+        fileDownloaded.GetContentFile('Crimes_-_2022 (1).csv')
+        df_crime = pd.read_csv('Crimes_-_2022 (1).csv', header=0, low_memory=False)
+    except:
+        print("Carga não conseguiu ser feita")
 
-        # dim_Time.insert(0, "id_time", id_Time, True)
+def clean_data():
+    # Vamos dropar as colunas X Coordinate, Y Coordinate, Latitude e Longitude
+    df_crime = df_crime.drop(['Latitude', 'Longitude', 'X Coordinate', 'Y Coordinate', 'Year'], axis=1)
+    # Limpar a coluna Location pois estava vindo com NaN
+    df_crime = df_crime.dropna(subset=['Location'], how='any')
+    df_crime
+    # dim_Time = df[['Date']].drop_duplicates()
+    # dim_Time = dim_Time.sort_values(by='Date', ascending=True)
 
-        # dim_Time
+    # id_Time = []
+    # id = 1
+    # for i in range(len(dim_Time)):
+    #     id_Time.append(id)
+    #     id += 1
+
+    # dim_Time.insert(0, "id_time", id_Time, True)
+
+    # dim_Time
 
 
     
--- transform.py
+++ transform.py
@@ -3,101 +3,101 @@ import datetime
 import holidays
 
 
-class Transform():
 
-    def is_weekend(self, day_str:str)->bool:
-        status = bool
-        dia, mes, ano = int, int, int
-        datetimme_splitv = day_str.split(' ')
-        partes_data = datetimme_splitv[0].split('/')
 
-        if len(partes_data) != 3:
-            print("Formato de data inválido. Use o formato dia/mês/ano.")
-            return None
+def is_weekend(day_str):
+    status = bool
+    dia, mes, ano = int, int, int
+    datetimme_splitv = day_str.split(' ')
+    partes_data = datetimme_splitv[0].split('/')
 
-        try:
-            dia = int(partes_data[0])
-            mes = int(partes_data[1])
-            ano = int(partes_data[2])
-        except ValueError:
-            print("Formato de data inválido. Certifique-se de que são números inteiros.")
+    if len(partes_data) != 3:
+        print("Formato de data inválido. Use o formato dia/mês/ano.")
+        return None
 
-        date_day = datetime.date(ano, dia, mes)
+    try:
+        dia = int(partes_data[0])
+        mes = int(partes_data[1])
+        ano = int(partes_data[2])
+    except ValueError:
+        print("Formato de data inválido. Certifique-se de que são números inteiros.")
 
-        if date_day.weekday() != 5 or date_day.weekday() != 6:
-            status = False
-        elif date_day.weekday() == 5 or date_day.weekday() == 6:
-            status = True 
-        return status
+    date_day = datetime.date(ano, dia, mes)
 
-    def is_holiday(self, day_str:str)->bool:
-        us_holidays = holidays(country='US')
-        dia, mes, ano = int, int, int
-        datetimme_splitv = day_str.split(' ')
-        partes_data = datetimme_splitv[0].split('/')
+    if date_day.weekday() != 5 or date_day.weekday() != 6:
+        status = False
+    elif date_day.weekday() == 5 or date_day.weekday() == 6:
+        status = True 
+    return status
 
-        if len(partes_data) != 3:
-            print("Formato de data inválido. Use o formato dia/mês/ano.")
-            return None
+def is_holiday(day_str):
+    us_holidays =  holidays.country_holidays('US')
+    dia, mes, ano = int, int, int
+    datetimme_splitv = day_str.split(' ')
+    partes_data = datetimme_splitv[0].split('/')
 
-        try:
-            dia = int(partes_data[0])
-            mes = int(partes_data[1])
-            ano = int(partes_data[2])
-        except ValueError:
-            print("Formato de data inválido. Certifique-se de que são números inteiros.")
+    if len(partes_data) != 3:
+        print("Formato de data inválido. Use o formato dia/mês/ano.")
+        return None
 
-        date_day = datetime.date(ano, dia, mes)
+    try:
+        dia = int(partes_data[0])
+        mes = int(partes_data[1])
+        ano = int(partes_data[2])
+    except ValueError:
+        print("Formato de data inválido. Certifique-se de que são números inteiros.")
 
-        satats = date_day in us_holidays
-        return satats
+    date_day = datetime.date(ano, dia, mes)
+
+    satats = date_day in us_holidays
+    return satats
+
+def time_to_shift(date_str):
+    horas, min, seg = int, int, int
+    period = str
+    datetime_split = date_str.split(' ')
+    partes_data = datetime_split[1].split(':')
+
+
+    if len(partes_data) != 3:
+        print("Formato de tempo inválido. Use o formato hora:min:seg.")
+        return None
+
+    try:
+        horas = int(partes_data[0])
+        min = int(partes_data[1])
+        seg = int(partes_data[2])
+    except ValueError:
+        print("Formato de tempo inválido. Certifique-se de que são números inteiros.")
+
+    if datetime_split[2]=='AM':
+        if 6 < horas <= 12:
+            period = "morning"
+        elif 0 < horas <= 6:
+            period = "dawn"
+
+    elif datetime_split[2]=='PM':
+        if 6 < horas <= 12:
+            period = "night"
+        elif 0 < horas <= 6:
+            period = "afternoon"
+
+    return period
+
+def ward_to_zone(ward):
+    zone = str
+    north = [26, 29, 30, 31, 32, 33, 35, 36, 37, 38, 39, 40, 41, 43, 44, 45, 46, 47, 48, 49, 50]
+    south = [5, 6, 7, 8, 9, 10, 17, 18, 19, 20, 21, 34]
+    center = [1, 2, 3, 4, 11, 12, 13, 14, 15, 16, 22, 23, 24, 25, 27, 28, 42]
+
+    if ward in north:
+        zone = 'north'
+    elif ward in center:
+        zone = 'center'
+    elif ward in south:
+        zone = 'south'
+    else:
+        print('Zona não registrada no catalogo de zonas' )
     
-    def time_to_shift(self, date_str:str)->str:
-        horas, min, seg = int, int, int
-        period = str
-        datetime_split = date_str.split(' ')
-        partes_data = datetime_split[1].split(':')
+    return zone
 
-
-        if len(partes_data) != 3:
-            print("Formato de tempo inválido. Use o formato hora:min:seg.")
-            return None
-
-        try:
-            horas = int(partes_data[0])
-            min = int(partes_data[1])
-            seg = int(partes_data[2])
-        except ValueError:
-            print("Formato de tempo inválido. Certifique-se de que são números inteiros.")
-
-        if datetime_split[2]=='AM':
-            if 6 < horas <= 12:
-                period = "morning"
-            elif 0 < horas <= 6:
-                period = "dawn"
-
-        elif datetime_split[2]=='PM':
-            if 6 < horas <= 12:
-                period = "night"
-            elif 0 < horas <= 6:
-                period = "afternoon"
-
-        return period
-    
-    def ward_to_zone(self, ward:int)->str:
-        zone = str
-        north = [26, 29, 30, 31, 32, 33, 35, 36, 37, 38, 39, 40, 41, 43, 44, 45, 46, 47, 48, 49, 50]
-        south = [5, 6, 7, 8, 9, 10, 17, 18, 19, 20, 21, 34]
-        center = [1, 2, 3, 4, 11, 12, 13, 14, 15, 16, 22, 23, 24, 25, 27, 28, 42]
-
-        if ward in north:
-            zone = 'north'
-        elif ward in center:
-            zone = 'center'
-        elif ward in south:
-            zone = 'south'
-        else:
-            print('Zona não registrada no catalogo de zonas' )
-        
-        return zone
-    
